25b16e69f5e18e84317729155f8f0884422b8a27
matthew rendely <matthew.rendely@gmail.com>
2023-04-15 09:29:55 -0700

    GET request setup

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/plants')
+    .then(r=>r.json())
+    .then(d=> {setPlants(d); console.log(d);})
+  },[])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
